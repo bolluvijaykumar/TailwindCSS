774505acf364ffa1f5b0aea2b02b1717bc036411
bolluvijaykumar <bolluvijaykumar@gmail.com>
2024-07-20 13:10:36 +0530

    Add files via upload

---
+++ firstproject/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "customBlue":"#1E40AF",
+        "customGreen":"#10B981"
+      }
+    },
+  },
+  plugins: [],
+}
+
